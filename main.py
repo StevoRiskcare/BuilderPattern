6bcf0899da449fc347fa080e8e2c7b8c46313a3d
StevoRiskcare <87176137+StevoRiskcare@users.noreply.github.com>
2024-03-11 16:54:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from Computers.accer import Accer
 from Computers.mac_book_air import MacBookAir
 from director import Director
@@ -14,8 +10,8 @@ def main():
     computer.display()
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
